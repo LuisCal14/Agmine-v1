a95a0ad92351ca9fd1be4195d9c05e049d1a9a23
Jhcronos <jhcronos@gmail.com>
2023-04-22 18:56:26 -0400

    toma

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './assets/style/App.css';
 import Nav from './components/Nav';
-import Home from './pages/HomePage'
+import Home from './pages/HomePage';
+import AboutUs from './pages/AboutUs';
 
 function App() {
 
   return (
     <div className="app">
       <Nav/>
-      <Home/>
+      <AboutUs/>
     </div>
   )
 }
