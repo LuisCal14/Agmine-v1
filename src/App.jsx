37d416cdd0a38ff56129cdb121566676019a3633
Aerialist006 <ljairolopez@gmail.com>
2023-05-02 14:22:00 -0400

    routing in frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "./assets/style/App.css";
 import HomePage from "./pages/HomePage";
 import Quizziz from "./pages/Quizziz";
@@ -9,7 +9,8 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="*" element={<MainPage Main={Quizziz} />} />
-        <Route path="/" element={<MainPage Main={HomePage} />} />
+        <Route path="/" element={<Navigate to="/home" replace={true}/>} />
+        <Route path="/home" element={<MainPage Main={HomePage} />} />
         <Route path="/quizziz" element={<MainPage Main={Quizziz} />} />
       </Routes>
     </Router>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,72 @@
 import React from "react";
-import { Link } from "react-router-dom"
-import './nav.css';
-import {GiHamburgerMenu} from 'react-icons/gi'
+import { Link } from "react-router-dom";
+import "./nav.css";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
-import Logo from '../../assets/images/logo.png'
+import Logo from "../../assets/images/logo.png";
 
 function Nav() {
-    
-    const [toogleMenu, setToogleMenu] = React.useState(false);
+  const [toogleMenu, setToogleMenu] = React.useState(false);
 
-    return (
+  return (
+    <div className="nav">
+      <div className="logo-container">
+        <Link to="/home">
+          <img src={Logo} alt="app logo" />
+        </Link>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/home/#home" className="nav-txt">
+            Home
+          </Link>
+        </li>
+        <li>
+          <a href="/home/#about" className="nav-txt">
+            About
+          </a>
+        </li>
+        <li>
+          <Link to="/quizziz">Games</Link>
+        </li>
+      </ul>
+      <div className="navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#9bf2ea"
+          fontSize={30}
+          onClick={() => setToogleMenu(true)}
+        ></GiHamburgerMenu>
 
-        <div className="nav">
-            <div className="logo-container"> 
-                <img src={Logo} alt="app logo"/>
-            </div>
-            <ul className="navbar-links">
-                <li><a href="#home" className="nav-txt">Home</a></li>
-                <li><a href="#about" className="nav-txt">About</a></li>
-                <li><a href="#" className="nav-txt">Games</a></li>
+        {toogleMenu && (
+          <div className="navbar-smallscreen_overlay flex__center slide-bottom">
+            <AiOutlineClose
+              fontSize={30}
+              color="#9bf2ea"
+              className="overlay__close"
+              onClick={() => setToogleMenu(false)}
+            />
+            <ul className="navbar-smallscreen_links">
+              <li className="p__opensans">
+                <Link to="/home/#home" onClick={() => setToogleMenu(false)}>
+                  Home
+                </Link>
+              </li>
+              <li className="p__opensans">
+                <Link to="/home/#about" onClick={() => setToogleMenu(false)}>
+                  About
+                </Link>
+              </li>
+              <li className="p__opensans">
+                <Link to="/quizziz" onClick={() => setToogleMenu(false)}>
+                  Games
+                </Link>
+              </li>
             </ul>
-            <div className="navbar-smallscreen">
-            <GiHamburgerMenu color='#9bf2ea' fontSize={30} onClick={() => setToogleMenu(true)}></GiHamburgerMenu>
-        
-                {toogleMenu && (
-                <div className='navbar-smallscreen_overlay flex__center slide-bottom'>
-                    <AiOutlineClose fontSize={30} color='#9bf2ea' className='overlay__close' onClick={() => setToogleMenu(false)}/> 
-                    <ul className='navbar-smallscreen_links'>
-                        <li className='p__opensans'><a href="#home">Home</a></li>
-                        <li className='p__opensans'><a href="#about">About</a></li>
-                        <li className='p__opensans'><Link to="/quizziz">Games</Link></li>
-                    </ul>
-                </div>
-                )}
-            </div>
-        </div>
-    )
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
-
-
 
 export default Nav;
